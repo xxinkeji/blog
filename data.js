81d45f74e3994c62351854006768bf01c91ea264
xxinkeji <151513795+xxinkeji@users.noreply.github.com>
2025-09-25 00:58:13 +0800

    Update data.js

--- data.js
+++ data.js
@@ -37,7 +37,7 @@ const certDatabase = [
   },
   // 在此下方添加新企业
   {
-    company: "北京世纪",
+    company: "北京",
     certId: "20240001",
     info: {
       category: "GB/T 17671-2021《建筑材料防火标准》",
@@ -48,6 +48,7 @@ const certDatabase = [
     }
   }
 ];
+
 
 
 
